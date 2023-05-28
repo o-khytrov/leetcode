6ade32d37a9034b48126a53ff53d3151a9385fb0
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2023-05-28 09:38:34 +0300

    1st commit

---
+++ functions.h
@@ -0,0 +1,13 @@
+//
+// Created by ok on 5/27/23.
+//
+#include <stdbool.h>
+
+#ifndef LEETCODE_FUNCTIONS_H
+#define LEETCODE_FUNCTIONS_H
+
+int strStr(char *haystack, char *needle);
+
+bool isSubsequence(char *s, char *t);
+
+#endif //LEETCODE_FUNCTIONS_H
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "functions.h"
+
+
+int main() {
+    char *s = "abc";
+    char *t = "ahbgdc";
+
+
+    printf("%d\n", isSubsequence(s, t));
+}
