17fafaddf57ddec939b198794b9dab0acbc6d421
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2023-08-30 08:50:10 +0300

    Search in a Binary Tree

---
+++ csharp/leetcode/leetcode/editor/en/[700]Search in a Binary Search Tree.cs
@@ -0,0 +1,78 @@
+/**
+You are given the root of a binary search tree (BST) and an integer val.
+
+ Find the node in the BST that the node's value equals val and return the
+subtree rooted with that node. If such a node does not exist, return null.
+
+
+ Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+
+ Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+
+
+
+ Constraints:
+
+
+ The number of nodes in the tree is in the range [1, 5000].
+ 1 <= Node.val <= 10⁷
+ root is a binary search tree.
+ 1 <= val <= 10⁷
+
+
+ Related Topics Tree Binary Search Tree Binary Tree 👍 5422 👎 175
+
+*/
+
+using Xunit;
+
+namespace SearchInABinarySearchTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    public class Solution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root is null)
+            {
+                return null;
+            }
+
+            if (root.val == val)
+                return root;
+            if (val > root.val)
+            {
+                return SearchBST(root.right, val);
+            }
+            else
+            {
+                return SearchBST(root.left, val);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
