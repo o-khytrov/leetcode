dea4ac2b84ef95a58fe14b3a38266c71b5ae649a
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2023-09-15 19:47:32 +0300

    2269. Find the K-Beauty of a Number

---
+++ csharp/leetcode/leetcode/editor/en/[2269]Find the K-Beauty of a Number.cs
@@ -0,0 +1,100 @@
+/**
+The k-beauty of an integer num is defined as the number of substrings of num
+when it is read as a string that meet the following conditions:
+
+
+ It has a length of k.
+ It is a divisor of num.
+
+
+ Given integers num and k, return the k-beauty of num.
+
+ Note:
+
+
+ Leading zeros are allowed.
+ 0 is not a divisor of any value.
+
+
+ A substring is a contiguous sequence of characters in a string.
+
+
+ Example 1:
+
+
+Input: num = 240, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "24" from "240": 24 is a divisor of 240.
+- "40" from "240": 40 is a divisor of 240.
+Therefore, the k-beauty is 2.
+
+
+ Example 2:
+
+
+Input: num = 430043, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "43" from "430043": 43 is a divisor of 430043.
+- "30" from "430043": 30 is not a divisor of 430043.
+- "00" from "430043": 0 is not a divisor of 430043.
+- "04" from "430043": 4 is not a divisor of 430043.
+- "43" from "430043": 43 is a divisor of 430043.
+Therefore, the k-beauty is 2.
+
+
+
+ Constraints:
+
+
+ 1 <= num <= 10⁹
+ 1 <= k <= num.length (taking num as a string)
+
+
+ Related Topics Math String Sliding Window 👍 571 👎 38
+
+*/
+
+using Xunit;
+
+namespace FindTheKBeautyOfANumber
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(240, 2, 2)]
+        [InlineData(430043, 2, 2)]
+        public void FindTheKBeautyOfANumberTest(int num, int k, int expectedResult)
+        {
+            Assert.Equal(expectedResult, new Solution().DivisorSubstrings(num, k));
+        }
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+    public class Solution
+    {
+        public int DivisorSubstrings(int num, int k)
+        {
+            var numStr = num.ToString();
+            var count = 0;
+
+            var l = 0;
+
+            while (l + k - 1 < numStr.Length)
+            {
+                var sub = numStr.Substring(l, k);
+                var subInt = int.Parse(sub);
+                if (subInt > 0 && num % subInt == 0)
+                {
+                    count++;
+                }
+
+                l++;
+            }
+
+            return count;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
