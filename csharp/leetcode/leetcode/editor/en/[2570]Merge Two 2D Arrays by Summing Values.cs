10e6b8ef2ab0e769c3cb8543e8bf7c316fc066ec
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2023-09-22 21:46:50 +0300

    2570. Merge Two 2D Arrays by Summing Values

---
+++ csharp/leetcode/leetcode/editor/en/[2570]Merge Two 2D Arrays by Summing Values.cs
@@ -0,0 +1,115 @@
+/**
+You are given two 2D integer arrays nums1 and nums2.
+
+
+ nums1[i] = [idi, vali] indicate that the number with the id idi has a value
+equal to vali.
+ nums2[i] = [idi, vali] indicate that the number with the id idi has a value
+equal to vali.
+
+
+ Each array contains unique ids and is sorted in ascending order by id.
+
+ Merge the two arrays into one array that is sorted in ascending order by id,
+respecting the following conditions:
+
+
+ Only ids that appear in at least one of the two arrays should be included in
+the resulting array.
+ Each id should be included only once and its value should be the sum of the
+values of this id in the two arrays. If the id does not exist in one of the two
+arrays then its value in that array is considered to be 0.
+
+
+ Return the resulting array. The returned array must be sorted in ascending
+order by id.
+
+
+ Example 1:
+
+
+Input: nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
+Output: [[1,6],[2,3],[3,2],[4,6]]
+Explanation: The resulting array contains the following:
+- id = 1, the value of this id is 2 + 4 = 6.
+- id = 2, the value of this id is 3.
+- id = 3, the value of this id is 2.
+- id = 4, the value of this id is 5 + 1 = 6.
+
+
+ Example 2:
+
+
+Input: nums1 = [[2,4],[3,6],[5,5]], nums2 = [[1,3],[4,3]]
+Output: [[1,3],[2,4],[3,6],[4,3],[5,5]]
+Explanation: There are no common ids, so we just include each id with its value
+in the resulting list.
+
+
+
+ Constraints:
+
+
+ 1 <= nums1.length, nums2.length <= 200
+ nums1[i].length == nums2[j].length == 2
+ 1 <= idi, vali <= 1000
+ Both arrays contain unique ids.
+ Both arrays are in strictly ascending order by id.
+
+
+ Related Topics Array Hash Table Two Pointers 👍 272 👎 9
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Xunit;
+
+namespace MergeTwo2DArraysBySummingValues
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData("[[1,2],[2,3],[4,5]]", "[[1,4],[3,2],[4,1]]", "[[1,6],[2,3],[3,2],[4,6]]")]
+        public void MergeTwo2DArraysBySummingValuesTest(string nums1Json, string nums2Json, string expectedResultJson)
+        {
+            var nums1 = JsonSerializer.Deserialize<int[][]>(nums1Json) ?? throw new ArgumentException("Invalid Json");
+            var nums2 = JsonSerializer.Deserialize<int[][]>(nums2Json) ?? throw new ArgumentException("Invalid Json");
+            var result = new Solution().MergeArrays(nums1, nums2);
+            Assert.Equal(expectedResultJson, JsonSerializer.Serialize(result));
+        }
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+    public class Solution
+    {
+        public int[][] MergeArrays(int[][] nums1, int[][] nums2)
+        {
+            var map = new Dictionary<int, int>();
+            for (var i = 0; i < nums1.Length; i++)
+            {
+                var id = nums1[i][0];
+                var value = nums1[i][1];
+                if (!map.TryAdd(id, value))
+                {
+                    map[id] += value;
+                }
+            }
+
+            for (var i = 0; i < nums2.Length; i++)
+            {
+                var id = nums2[i][0];
+                var value = nums2[i][1];
+                if (!map.TryAdd(id, value))
+                {
+                    map[id] += value;
+                }
+            }
+
+            return map.OrderBy(x => x.Key).Select(x => new int[] { x.Key, x.Value }).ToArray();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
