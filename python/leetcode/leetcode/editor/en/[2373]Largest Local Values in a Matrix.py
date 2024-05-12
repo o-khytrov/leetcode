2b3b8466ea60f3a21194cce6d7101e86850e503d
Oleksandr Khytrov <o.khytrov@gmail.com>
2024-05-12 16:36:11 +0300

    2373. Largest Local Values in a Matrix

---
+++ python/leetcode/leetcode/editor/en/[2373]Largest Local Values in a Matrix.py
@@ -0,0 +1,104 @@
+# You are given an n x n integer matrix grid. 
+# 
+#  Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that: 
+# 
+#  
+#  maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid 
+# centered around row i + 1 and column j + 1. 
+#  
+# 
+#  In other words, we want to find the largest value in every contiguous 3 x 3 
+# matrix in grid. 
+# 
+#  Return the generated matrix. 
+# 
+#  
+#  Example 1: 
+#  
+#  
+# Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+# Output: [[9,9],[8,6]]
+# Explanation: The diagram above shows the original matrix and the generated 
+# matrix.
+# Notice that each value in the generated matrix corresponds to the largest 
+# value of a contiguous 3 x 3 matrix in grid. 
+# 
+#  Example 2: 
+#  
+#  
+# Input: grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+# Output: [[2,2,2],[2,2,2],[2,2,2]]
+# Explanation: Notice that the 2 is contained within every contiguous 3 x 3 
+# matrix in grid.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  n == grid.length == grid[i].length 
+#  3 <= n <= 100 
+#  1 <= grid[i][j] <= 100 
+#  
+# 
+#  Related Topics Array Matrix 👍 876 👎 107
+
+import unittest
+from typing import List
+
+
+class LargestLocalValuesInAMatrix_Test(unittest.TestCase):
+
+    def test_largestLocalValuesInAMatrix(self):
+        test_data = [
+            ([[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]], [[9, 9], [8, 6]]),
+            ([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 2, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]],
+             [[2, 2, 2], [2, 2, 2], [2, 2, 2]])
+        ]
+        for grid, expected in test_data:
+            with self.subTest(grid=grid, expected=expected):
+                result = Solution().largestLocal(grid)
+                for expected_row, actual_row in zip(expected, result):
+                    self.assertSequenceEqual(expected_row, actual_row)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        result = []
+
+        def get_safe(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]):
+                return 0
+            return grid[i][j]
+
+        def get_max_value(i, j):
+            return max(
+                get_safe(i, j),
+                get_safe(i + 1, j),
+                get_safe(i - 1, j),
+
+                get_safe(i, j + 1),
+                get_safe(i, j - 1),
+
+                get_safe(i + 1, j + 1),
+                get_safe(i + 1, j - 1),
+
+                get_safe(i - 1, j + 1),
+                get_safe(i - 1, j - 1),
+            )
+
+        for i in range(n - 2):
+            result.append([])
+            for j in range(n - 2):
+                val = get_max_value(i + 1, j + 1)
+                result[-1].append(val)
+
+        return result
+
+# leetcode submit region end(Prohibit modification and deletion)
