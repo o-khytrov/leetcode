ccc800d61214c4a5bd8a41764edd966b143dd7fb
Oleksandr Khytrov <o.khytrov@gmail.com>
2024-05-29 09:36:42 +0300

    1404. Number of Steps to Reduce a Number in Binary Representation to One

---
+++ python/leetcode/leetcode/editor/en/[1404]Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -0,0 +1,86 @@
+# Given the binary representation of an integer as a string s, return the 
+# number of steps to reduce it to 1 under the following rules: 
+# 
+#  
+#  If the current number is even, you have to divide it by 2. 
+#  If the current number is odd, you have to add 1 to it. 
+#  
+# 
+#  It is guaranteed that you can always reach one for all test cases. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "1101"
+# Output: 6
+# Explanation: "1101" corressponds to number 13 in their decimal representation.
+# 
+# Step 1) 13 is odd, add 1 and obtain 14. 
+# Step 2) 14 is even, divide by 2 and obtain 7.
+# Step 3) 7 is odd, add 1 and obtain 8.
+# Step 4) 8 is even, divide by 2 and obtain 4.  
+# Step 5) 4 is even, divide by 2 and obtain 2. 
+# Step 6) 2 is even, divide by 2 and obtain 1.  
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "10"
+# Output: 1
+# Explanation: "10" corressponds to number 2 in their decimal representation.
+# Step 1) 2 is even, divide by 2 and obtain 1.  
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: s = "1"
+# Output: 0
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 500 
+#  s consists of characters '0' or '1' 
+#  s[0] == '1' 
+#  
+# 
+#  Related Topics String Bit Manipulation 👍 962 👎 71
+
+import unittest
+
+
+class NumberOfStepsToReduceANumberInBinaryRepresentationToOne_Test(unittest.TestCase):
+
+    def test_numberOfStepsToReduceANumberInBinaryRepresentationToOne(self):
+        test_data = [
+            ("1101", 6),
+            ("10", 1)
+        ]
+        for s, expected in test_data:
+            with self.subTest(s=s, expected=expected):
+                self.assertEqual(expected, Solution().numSteps(s))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numSteps(self, s: str) -> int:
+        count = 0
+        s = int(s, 2)
+        while s != 1:
+            if s % 2 == 0:
+                s = s // 2
+            else:
+                s += 1
+            count += 1
+        return count
+
+# leetcode submit region end(Prohibit modification and deletion)
